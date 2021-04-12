4e04467e19e447d30a74ae6249029b09a77be494
stuartdavis113 <55687735+stuartdavis113@users.noreply.github.com>
2021-04-12 20:20:29 +0100

    Add files via upload

---
+++ org.mde.spec.xpect/src/org/mde/spec/xpect/XPectTests.java
@@ -0,0 +1,10 @@
+package org.mde.spec.xpect;
+
+import org.eclipse.xpect.runner.XpectRunner;
+import org.eclipse.xpect.xtext.lib.tests.XtextTests;
+import org.junit.runner.RunWith;
+
+@RunWith(XpectRunner.class)
+public class XPectTests extends XtextTests {
+
+}
